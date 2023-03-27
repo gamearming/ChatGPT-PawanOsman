07d6b98236fb38f99e9f47a30a2f493bced540f9
gamearming <gamearming@hotmail.com>
2023-03-27 14:23:41 +0800

    加入繁體中文註解

--- middlewares.js
+++ middlewares.js
@@ -1,7 +1,17 @@
+/**
+ * 這個模組包含了兩個中介軟體函數，一個是處理跨來源資源共用 (CORS)，另一個是實現 IP 請求速率限制。
+ * @module middleware
+ */
+
 import { RATE_LIMIT, PRIOD, WHITELISTED_IPS } from "./config.js";
 
-const rateLimit = new Map();
-
+/**
+ * 處理跨來源資源共用的中介軟體函數。
+ *
+ * @param {object} req - Node.js 的 HTTP 請求物件。
+ * @param {object} res - Node.js 的 HTTP 回應物件。
+ * @param {function} next - Express.js 框架中的下一個中介軟體函數。
+ */
 function corsMiddleware(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
@@ -9,6 +19,14 @@ function corsMiddleware(req, res, next) {
     next();
 };
 
+/**
+ * 實現 IP 請求速率限制的中介軟體函數。
+ *
+ * @param {object} req - Node.js 的 HTTP 請求物件。
+ * @param {object} res - Node.js 的 HTTP 回應物件。
+ * @param {function} next - Express.js 框架中的下一個中介軟體函數。
+ * @throws {object} 如果 IP 發送太多請求，則拋出包含錯誤訊息的物件。
+ */
 async function rateLimitMiddleware(req, res, next) {
     let ip = req.headers["CF-Connecting-IP"] ?? req.headers["cf-connecting-ip"] ?? req.headers["X-Forwarded-For"] ?? req.headers["x-forwarded-for"] ?? req.src.ip;
     if (WHITELISTED_IPS.includes(ip)) return next();
@@ -43,4 +61,4 @@ async function rateLimitMiddleware(req, res, next) {
     next();
 };
 
-export { corsMiddleware, rateLimitMiddleware }
+export { corsMiddleware, rateLimitMiddleware };
