7f355eac134dd47c4a92ca13f007654a86586b64
gamearming <gamearming@hotmail.com>
2023-03-27 14:02:19 +0800

    加入繁體中文註解

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
+/**
+ * @fileoverview 這是 BAI Chat 的伺服器端程式碼，使用 Express 建立 Web API。
+ * @author The BAI LLC
+ * @version 0.1
+ */
+
 import express, { json, urlencoded } from 'express';
 import { completions, chatCompletions } from './routes.js';
 import { corsMiddleware, rateLimitMiddleware } from './middlewares.js';
 import { SERVER_PORT } from './config.js';
 
+/**
+ * 使用 Express 建立應用程式對象。
+ */
 let app = express();
 
 process.on("uncaughtException", function (err) {
     if (DEBUG) console.error(`Caught exception: ${err}`);
 });
 
-// Middlewares
+/**
+ * 跨域中間件
+ * @function
+ * @param {Object} req - Express 請求對象
+ * @param {Object} res - Express 回應對象
+ * @param {Function} next - Express 中介軟體支援的下一個函數
+ */
 app.use(corsMiddleware);
+
+/**
+ * 請求限制中間件
+ * @function
+ * @param {Object} req - Express 請求對象
+ * @param {Object} res - Express 回應對象
+ * @param {Function} next - Express 中介軟體支援的下一個函數
+ */
 app.use(rateLimitMiddleware);
+
+// 解析 JSON 內容的中間件
 app.use(json());
+
+// 解析 URL 編碼內容的中間件
 app.use(urlencoded({ extended: true }));
 
-// Register routes
+/**
+ * 回傳聊天 API 相關資訊
+ * @function
+ * @param {Object} req - Express 請求對象
+ * @param {Object} res - Express 回應對象
+ * @returns {Object} 包含聊天 API 相關資訊的 JSON 物件
+ */
 app.all("/", async function (req, res) {
     res.set("Content-Type", "application/json");
     return res.status(200).send({
@@ -24,10 +57,18 @@ app.all("/", async function (req, res) {
         discord: "https://discord.pawan.krd"
     });
 });
+
+// 註冊自動完成路由
 app.post("/v1/completions", completions);
+
+// 註冊聊天自動完成路由
 app.post("/v1/chat/completions", chatCompletions);
 
-// Start server
+/**
+ * 啟動伺服器，監聽指定端口
+ * @function
+ * @param {number} SERVER_PORT - 伺服器監聽的端口號
+ */
 app.listen(SERVER_PORT, () => {
     console.log(`Listening on ${SERVER_PORT} ...`);
 });
